5b8c383bb6837f0bb914f010c643d7294ca50074
unknown <894824594@qq.com>
2022-08-11 13:44:32 +0800

    AddDataImport

---
+++ src/WicresoftForumApi/WicresoftForumDataImport/BasicImport/BasicDataImporter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WicresoftForumDataImport.Utils;
+using WicresoftForumApi.Data;
+
+namespace WicresoftForumDataImport.BasicImport
+{
+    class BasicDataImporter
+    {
+        private static readonly List<string> _mailPostfixes = new()
+        {
+            "163.com"
+        };
+
+        public static void UserImport()
+        {
+            var repo = CommonUtils.GetRepo();
+            var userNames = new[]
+            {
+                "Admin",
+                "张三",
+                "李四",
+                "王五",
+            };
+            var users = new List<User>();
+            foreach (var userName in userNames)
+            {
+                users.Add(ConstructUser(userName));
+            }
+            _ = repo.ReFillUsers(users).Result;
+        }
+
+        private static User ConstructUser(string userName)
+        {
+            var mailPostfix = _mailPostfixes.OrderBy(item => Guid.NewGuid()).FirstOrDefault();
+            return new User
+            {
+                UserName = userName,
+                Password = "1",
+                Email = $"{userName}@{mailPostfix}",
+                UserType = userName == "admin" ? 0 : 1,
+                CreateTime = DateTime.Now
+            };
+        }
+    }
+}
---
+++ src/WicresoftForumApi/WicresoftForumDataImport/Program.cs
@@ -0,0 +1,13 @@
+﻿using WicresoftForumDataImport.BasicImport;
+
+namespace WicresoftForumDataImport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            BasicDataImporter.UserImport();
+        }
+    }
+}
